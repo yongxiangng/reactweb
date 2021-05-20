b94c88373757a4bdda719fb3d659a56e1ab444dc
yongxiangng <yongxiangng@gmail.com>
2021-05-20 17:33:47 +0800

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+
+import logo from "./logo.svg";
+
+import "./components/FontAwesomeIcon";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "./Footer.css";
+
+export default function Footer() {
+  const contacts = [
+    {
+      icon: ["fab", "github"],
+      url: "github.com/yongxiangng",
+      isWebPage: true,
+    },
+    {
+      icon: ["fab", "linkedin"],
+      url: "linkedin.com/in/ngyongxiang",
+      isWebPage: true,
+    },
+    {
+      icon: ["fas", "envelope"],
+      url: "e0388929@u.nus.edu",
+      isWebPage: false,
+    },
+  ];
+
+  return (
+    <div className="Footer">
+      <div className="footerMenu">
+        <div className="contacts">
+          {contacts.map((contact) => (
+            <div className="contact">
+              <div className="icon">
+                <FontAwesomeIcon icon={contact.icon} className="fa" />
+              </div>
+              <a
+                className="contact-url"
+                href={contact.isWebPage ? "https://" + contact.url : null}
+                target="_blank"
+              >
+                {contact.url}
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
