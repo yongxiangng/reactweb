56edc231dd145d7c0a39e4dfacd2feb9dd66ce49
Ng Yong Xiang <60974969+yongxiangng@users.noreply.github.com>
2021-05-21 22:24:23 +0800

    About (#2)
    * Run Prettier
    
    * About page skeleton
    
    * About page
    
    * Fix alignment issue
    
    * Resolve home formatting issue

--- src/pages/Awards.js
+++ src/pages/Awards.js
@@ -1,7 +1,28 @@
+import PageHeader from "../components/PageHeader";
+
 function Awards() {
   return (
-    <div>
-      <p>Content page</p>
+    <div className="about-container">
+      <div className="about-header">
+        <PageHeader name="About Me" />
+      </div>
+      <div className="about-content">
+        <p className="about-content-text">
+          {`Graduated from NUS High in 2019, I started 
+          to pursue my degree at NUS in 2020 as a Computer Science 
+          and Business Administration Double Degree student to further 
+          my interest in Computer Science and Finance.`}
+          <br />
+          <br />
+
+          {`I have been working on a few projects. Do check them out 
+          under the projects tab!`}
+          <br />
+          <br />
+
+          {`Thanks for reading this :3`}
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,63 +1,60 @@
 import "./Home.css";
 
 import pic1 from "../assets/home/imgbin_doraemon-animation-png.png";
-import pic2 from "../assets/home/toppng.com-computer-science-png-women-symbol-in-science-813x708.png"
+import pic2 from "../assets/home/toppng.com-computer-science-png-women-symbol-in-science-813x708.png";
+
 function Home() {
-	return (
-		<div className="home">
-			<HomeCard1 parity="0" />
-			<HomeCard2 parity="1" />
-		</div>
-	);
+  return (
+    <div className="home-container">
+      <HomeCard1 parity="0" />
+      <HomeCard2 parity="1" />
+    </div>
+  );
 }
 
 // chose to hard code to prevent use of innerHTML, anyways my Home Page doesn't change often
 
 function HomeCard1(props) {
-	const parity = props.parity;
-	return (
-		<div className="home-card">
-			<div className="home-content-container">
-				<p className="home-content">
-					Hello I am
-        <br />
-					<strong>
-						<span className="name">Ng Yong Xiang</span>
-					</strong>
-					<br />
-        Year 2 NUS Computing + Business Administration DDP student
-      </p>
-			</div>
-			<div className={parity === "0" ? "picture-container0" : "picture-container1"}>
-				<img
-					src={pic1}
-					alt="Home page"
-					className="picture"
-				></img>
-			</div>
-		</div>
-	);
+  const parity = props.parity;
+  return (
+    <div className="home-card">
+      <div className="home-content-container">
+        <p className="home-content">
+          Hello I am
+          <br />
+          <strong>
+            <span className="name">Ng Yong Xiang</span>
+          </strong>
+          <br />
+          Year 2 NUS Computing + Business Administration DDP student
+        </p>
+      </div>
+      <div
+        className={parity === "0" ? "picture-container0" : "picture-container1"}
+      >
+        <img src={pic1} alt="Home page" className="picture"></img>
+      </div>
+    </div>
+  );
 }
 
 function HomeCard2(props) {
-	const parity = props.parity;
-	return (
-		<div className="home-card">
-			<div className={parity === "0" ? "picture-container0" : "picture-container1"}>
-				<img
-					src={pic2}
-					alt="Home page"
-					className="picture"
-				></img>
-			</div>
-			<div className="home-content-container">
-				<p className="home-content">
-					The purpose of the website is for me to consolidate all my projects and deploy them.
-					I really don't know what else to write here.
-      </p>
-			</div>
-		</div>
-	);
+  const parity = props.parity;
+  return (
+    <div className="home-card">
+      <div
+        className={parity === "0" ? "picture-container0" : "picture-container1"}
+      >
+        <img src={pic2} alt="Home page" className="picture"></img>
+      </div>
+      <div className="home-content-container">
+        <p className="home-content">
+          The purpose of the website is for me to consolidate all my projects
+          and deploy them. I really don't know what else to write here.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
