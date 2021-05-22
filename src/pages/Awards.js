3c2f4eb9755117e2736d2fbb7d3f897102a1d87b
Ng Yong Xiang <60974969+yongxiangng@users.noreply.github.com>
2021-05-22 23:43:11 +0800

    Awards (#6)
    * Awards page
    
    * Prettier

--- src/pages/Awards.js
+++ src/pages/Awards.js
@@ -1,27 +1,34 @@
+import "./Awards.css";
+
 import PageHeader from "../components/PageHeader";
+
+const awards = require("../database/awards.json");
 
 function Awards() {
   return (
-    <div className="about-container">
-      <div className="about-header">
-        <PageHeader name="About Me" />
+    <div className="awards-container">
+      <PageHeader name="Awards" />
+      <ol className="awards-list">
+        {awards.map((award, index) => (
+          <AwardCard award={award} key={index} />
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+function AwardCard(props) {
+  const { award } = props;
+  const { title, date, description } = award;
+
+  return (
+    <div className="award-card">
+      <div className="award-card-header">
+        <h2>{title}</h2>
+        <h2>{date}</h2>
       </div>
-      <div className="about-content">
-        <p className="about-content-text">
-          {`Graduated from NUS High in 2019, I started 
-          to pursue my degree at NUS in 2020 as a Computer Science 
-          and Business Administration Double Degree student to further 
-          my interest in Computer Science and Finance.`}
-          <br />
-          <br />
-
-          {`I have been working on a few projects. Do check them out 
-          under the projects tab!`}
-          <br />
-          <br />
-
-          {`Thanks for reading this :3`}
-        </p>
+      <div className="award-card-description">
+        <p>{description}</p>
       </div>
     </div>
   );
