418091afefe3360b0f48a404a540744c8c9a9fc6
yongxiangng <yongxiangng@gmail.com>
2021-05-28 22:33:23 +0800

    Responsive navigation bar feature

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,33 +2,16 @@ import styles from "./Header.module.css";
 
 import logo from "../../assets/logo.svg";
 
+import { useState } from "react";
+
 import { Link } from "react-router-dom";
 
 export default function Header() {
-  const buttons = [
-    {
-      name: "Home",
-      link: "/",
-    },
-    {
-      name: "About",
-      link: "/about",
-    },
-    {
-      name: "Projects",
-      link: "/projects",
-    },
-    {
-      name: "Awards",
-      link: "/awards",
-    },
-  ];
-
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <Logo />
-        <NavigationBar buttons={buttons} />
+        <NavigationBar />
       </div>
     </div>
   );
@@ -42,24 +25,114 @@ function Logo() {
   );
 }
 
-function NavigationBar(props) {
-  const { buttons } = props;
+function NavigationBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
   return (
-    <ol className={styles.navigationBar}>
-      {buttons.map((button) => (
-        <Button button={button} key={button.name} />
-      ))}
-    </ol>
-  );
-}
+    <>
+      <header className="lg:px-16 px-6 flex flex-wrap items-start lg:py-0 py-2">
+        <div className="lg:hidden flex items-start">
+          {isOpen ? (
+            <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+              <li>
+                <Link
+                  to="/"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-200"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/about"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-200"
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/projects"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-200"
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/awards"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-200"
+                >
+                  Awards
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <></>
+          )}
+          <button
+            className="mobile-menu-button pl-6 pt-7"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <svg
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
 
-function Button(props) {
-  const { button } = props;
-  return (
-    <li className={styles.button}>
-      <Link to={button.link} className={styles.buttonText}>
-        {button.name}
-      </Link>
-    </li>
+        <div
+          className="hidden lg:flex lg:items-center lg:w-auto w-full"
+          id="menu"
+        >
+          <nav>
+            <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+              <li>
+                <Link
+                  to="/"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-200"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/about"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-200"
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/projects"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-200"
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/awards"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-blue-200"
+                >
+                  Awards
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
   );
 }
