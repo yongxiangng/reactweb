65229706512205442e56bcce91b29d05f062cfca
yongxiangng <yongxiangng@gmail.com>
2021-06-04 08:52:18 +0800

    Antd header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -8,28 +8,52 @@ import Project from "./pages/Project";
 import Awards from "./pages/Awards";
 
 import "./components/FontAwesomeIcon";
-import "./App.css";
+import "./App.less";
 import "antd/dist/antd.less";
 
+import logo from "./assets/logo.svg";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Layout, Menu } from 'antd';
+import { Link } from "react-router-dom";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="app-container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/projects" component={Projects} />
-            <Route path="/projects/:id" component={Project} />
-            <Route exact path="/awards" component={Awards} />
-          </Switch>
+    <Layout className="layout">
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <div style={{width:'1200px'}}>
+          <div className="logo">
+            <Link to="/">
+              <img src={logo} alt="Logo"></img>
+            </Link>
+          </div>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{float: 'right'}}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+            <Menu.Item key="4">nav 4</Menu.Item>
+          </Menu>
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Header>
+      <Router>
+        <div className="App">
+          <Content>
+          <div className="app-container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/projects" component={Projects} />
+              <Route path="/projects/:id" component={Project} />
+              <Route exact path="/awards" component={Awards} />
+            </Switch>
+          </div>
+        </Content>
+        </div>
+      </Router>
+      <Footer />
+    </Layout>
   );
 }
 
